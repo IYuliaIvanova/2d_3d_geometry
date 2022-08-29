7bc6e2c8546c1e20d8ac63a9779dde66b0a142cc
Yulia Ivanova <sheeeellby@gmail.com>
2022-08-29 12:23:06 +0300

    feature: constructor Matrix, Sphere

--- js/script.js
+++ js/script.js
@@ -10,12 +10,14 @@ Vector2.prototype.length = function () {
 Vector2.prototype.add = function (otherVector) {
   this.x += otherVector.x;
   this.y += otherVector.y;
+
   return this;
 };
 
 Vector2.prototype.sub = function (otherVector) {
   this.x -= otherVector.x;
   this.y -= otherVector.y;
+
   return this;
 };
 
@@ -33,6 +35,7 @@ Vector3.prototype.add = function (otherVector) {
   this.x += otherVector.x;
   this.y += otherVector.y;
   this.z += otherVector.z;
+
   return this;
 };
 
@@ -40,6 +43,15 @@ Vector3.prototype.sub = function (otherVector) {
   this.x -= otherVector.x;
   this.y -= otherVector.y;
   this.z -= otherVector.z;
+
+  return this;
+};
+
+Vector3.prototype.multiplyByScalar = function (scalar) {
+  this.x *= scalar;
+  this.y *= scalar;
+  this.z *= scalar;
+
   return this;
 };
 
@@ -56,9 +68,10 @@ LineSegment.prototype.length = function () {
   );
 };
 
-function Dot(x, y) {
+function Point(x, y, z = 0) {
   this.x = x;
   this.y = y;
+  this.z = z;
 }
 
 function Polygon(...vectors) {
@@ -79,6 +92,45 @@ Polygon.prototype.square = function () {
   return Math.abs(result) / 2;
 };
 
+function Matrix(matrix) {
+  this.matrix = matrix;
+}
+
+Matrix.prototype.multiplyMatrix = function (matrixB) {
+  const result = new Array(this.matrix.length);
+
+  for (let row = 0; row < this.matrix.length; row++) {
+    result[row] = new Array(matrixB[0].length);
+
+    for (let column = 0; column < matrixB[0].length; column++) {
+      result[row][column] = 0;
+
+      for (let i = 0; i < this.matrix[0].length; i++) {
+        result[row][column] += this.matrix[row][i] * matrixB[i][column];
+      }
+    }
+  }
+
+  return result;
+};
+
+function Sphere(center = new Vector3(), radius) {
+  this.center = center;
+  this.radius = radius;
+}
+
+Sphere.prototype.square = function () {
+  return 4 * Math.PI * Math.pow(this.radius, 2);
+};
+
+Sphere.prototype.distanceToPoint = function (point = new Point()) {
+  return Math.sqrt(
+    Math.pow(point.x - this.center.x, 2) +
+      Math.pow(point.y - this.center.y, 2) +
+      Math.pow(point.z - this.center.z, 2)
+  );
+};
+
 function distance(obj1, obj2) {
   if (obj1.hasOwnProperty("z") && obj2.hasOwnProperty("z")) {
     return Math.sqrt(
@@ -91,14 +143,27 @@ function distance(obj1, obj2) {
 }
 
 function init() {
-  console.log(new Vector2(1, 2));
-  console.log(distance(new Vector3(4, -6, 3), new Vector3(-1, 5, -4)));
-  console.log(distance(new Dot(1, 1), new Vector2(1, 3)));
-  console.log(new Vector2(2, 3).add(new Vector2(1, 1)));
-  console.log(new Vector2(1, 2).length());
-  console.log(new LineSegment(2, 4, 4, 1).length());
-  console.log(new Polygon([1, 2], [2, 4]));
-  console.log(new Polygon([2, 4], [3, -8], [1, 2], [2, 4]).square());
+  //   console.log(new Vector2(1, 2));
+  //   console.log(distance(new Vector3(4, -6, 3), new Vector3(-1, 5, -4)));
+  //   console.log(distance(new Point(1, 1), new Vector2(1, 3)));
+  //   console.log(new Vector2(2, 3).add(new Vector2(1, 1)));
+  //   console.log(new Vector2(1, 2).length());
+  //   console.log(new LineSegment(2, 4, 4, 1).length());
+  //   console.log(new Polygon([1, 2], [2, 4]));
+  //   console.log(new Polygon([2, 4], [3, -8], [1, 2], [2, 4]).square());
+  //   console.log(
+  //     new Sphere({ x: 4, y: -6, z: 3 }, 4).distanceToPoint({ x: -1, y: 5, z: -4 })
+  //   );
+  console.log(
+    new Matrix([
+      [2, 0, -1],
+      [0, -2, 2],
+    ]).multiplyMatrix([
+      [4, 1, 0],
+      [3, 2, 1],
+      [0, 1, 0],
+    ])
+  );
 }
 
 init();
